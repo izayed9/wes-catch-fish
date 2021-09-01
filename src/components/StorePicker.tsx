1f6e93e512fead746887c2d3eb45577b504d7e94
izayed9 <izayed9@gmail.com>
2021-09-01 23:08:25 +0600

    fixed input field

--- src/components/StorePicker.tsx
+++ src/components/StorePicker.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import{getFunName} from '../helpers/helpers'
 
 const StorePicker = () => {
+    const myInput = React.createRef<HTMLInputElement>()
+    const goToStore = (e) => {
+        e.preventDefault()
+        const storeName = myInput.current.value;
+
+   }
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={goToStore}>
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder="Store Name" />
-            <button type="submit">Visit Store →</button>
+            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ myInput}/>
+            <button type="submit"  >Visit Store →</button>
         </form>
     )
 }
